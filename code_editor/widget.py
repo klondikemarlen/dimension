52236af66f22ddd64f0644d96caf89496de4b22e
Marlen Brunner <klondikemarlen@gmail.com>
2018-07-11 11:56:50 -0700

    Add new code block via double tap.
    Finally fix bug where setting 'on_touch_down' in CodeEditor broke movement in CodeBlocks.

--- code_editor/widget.py
+++ code_editor/widget.py
@@ -1,9 +1,17 @@
 from kivy.uix.widget import Widget
+# from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
+from code_block.widget import CodeBlock
 
 
 class CodeEditor(Widget):
     block = ObjectProperty(None)
 
-    # def on_touch_move(self, touch):
-    #     touch.ud['line'].points += [touch.x, touch.y]
+    def on_touch_down(self, touch):
+        for child in self.children:
+            if child.on_touch_down(touch):
+                break
+        if touch.is_double_tap:
+            block = CodeBlock()
+            block.center = (touch.x, touch.y)
+            self.add_widget(block)
