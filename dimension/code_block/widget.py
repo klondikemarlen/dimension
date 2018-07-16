a7c6c9a860270302e2d578d8c2a9ff7775707e69
Marlen Brunner <klondikemarlen@gmail.com>
2018-07-16 16:30:48 -0700

    Found a way to replace most of my code with a Scatter widget.
    I am using a ScatterPlane to avoid having dead space.
    I'm then transforming all the child blocks.
    This introduces the bug of having blocks move off screen.
    
    But it is a start!

--- dimension/code_block/widget.py
+++ dimension/code_block/widget.py
@@ -1,25 +1,31 @@
 import pdb
 
 from kivy.uix.widget import Widget
+from kivy.uix.scatter import Scatter
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import BooleanProperty, ReferenceListProperty, NumericProperty
+from kivy.properties import BooleanProperty, ReferenceListProperty, NumericProperty, ObjectProperty
 
 
-class CodeBlock(Widget):
+class CodeBlock(FloatLayout):
+    scatter = ObjectProperty(None)
     selected = BooleanProperty(False)
     default_width = NumericProperty(0)
     default_height = NumericProperty(0)
     default_size = ReferenceListProperty(default_width, default_height)
     scale_factor = NumericProperty(0)
 
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            self.selected = True
-            return True
+    # def on_touch_down(self, touch):
+    #     if self.collide_point(*touch.pos):
+    #         self.selected = True
+    #         return True
+    #
+    # def on_touch_up(self, touch):
+    #     self.selected = False
+    #
+    # def on_touch_move(self, touch):
+    #     if self.selected:
+    #         self.center = self.center_x + touch.dx, self.center_y + touch.dy
 
-    def on_touch_up(self, touch):
-        self.selected = False
-
-    def on_touch_move(self, touch):
-        if self.selected:
-            self.center = self.center_x + touch.dx, self.center_y + touch.dy
+    def apply_transform(self, *args, **kwargs):
+        self.scatter.apply_transform(*args, **kwargs)
--- dimension/code_editor/widget.py
+++ dimension/code_editor/widget.py
@@ -1,63 +1,74 @@
 import pdb
 
 from kivy.uix.widget import Widget
-# from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty, NumericProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatterlayout import ScatterLayout, ScatterPlaneLayout
+from kivy.uix.scatter import ScatterPlane
+from kivy.properties import ObjectProperty, NumericProperty, ListProperty
 from dimension.code_block.widget import CodeBlock
 
 
 class CodeEditor(Widget):
+    scatter_plane = ObjectProperty()
     selected_block = ObjectProperty(None, allownone=True)
     grid_scale = NumericProperty(0)
     half_scale = NumericProperty(0)
     block_scale = NumericProperty(0)
+    blocks = ListProperty()
 
     def on_touch_down(self, touch):
         if touch.is_double_tap:
-            block = CodeBlock(name="Block {}".format(len(self.children)+1), scale_factor=self.block_scale)
+            block = CodeBlock()
+            block.name = "Block {}".format(len(self.blocks)+1)
+            block.scale_factor = self.block_scale
             block.center = touch.pos
             self.add_widget(block)
+            self.blocks.append(block)
 
         for child in self.children:
             if child.on_touch_down(touch):
-                self.selected_block = child
+                # self.selected_block = child
                 break
 
-    def on_touch_up(self, touch):
-        # Prevent intersection/blocks can't stop on top of each other?
-        if self.selected_block:
-            self.snap_to_grid(self.selected_block)
+    def apply_transform(self, *args, **kwargs):
+        for block in self.blocks:
+            block.apply_transform(*args, **kwargs)
 
-            # work out how to prevent overlap ...
-            unselected_children = (child for child in self.children if child is not self.selected_block)
-            for child in unselected_children:
-                if self.selected_block.collide_widget(child):
-                    print(child.name)
-                    # Decide whether to nudge both or just the selected or child?
-
-        for child in self.children:
-            if child.on_touch_up(touch):
-                break
-
-        self.selected_block = None
-
-    def snap_to_grid(self, child):
-        x, y = child.pos
-
-        # Snap child to grid positions.
-        # There is probably a better way?
-        x_grid = x // self.grid_scale
-        y_grid = y // self.grid_scale
-        x = x_grid * self.grid_scale if x % self.grid_scale < self.half_scale else (x_grid + 1) * self.grid_scale
-        y = y_grid * self.grid_scale if y % self.grid_scale < self.half_scale else (y_grid + 1) * self.grid_scale
-
-        # Keep child inside of border
-        if x + child.width > self.width:
-            x = self.width - child.width
-        if x < 0:
-            x = 0
-        if y + child.height > self.height:
-            y = self.height - child.height
-        if y < 0:
-            y = 0
-        child.pos = x, y
+    # def on_touch_up(self, touch):
+    #     # Prevent intersection/blocks can't stop on top of each other?
+    #     if self.selected_block:
+    #         self.snap_to_grid(self.selected_block)
+    #
+    #         # work out how to prevent overlap ...
+    #         unselected_children = (child for child in self.children if child is not self.selected_block)
+    #         for child in unselected_children:
+    #             if self.selected_block.collide_widget(child):
+    #                 print(child.name)
+    #                 # Decide whether to nudge both or just the selected or child?
+    #
+    #     for child in self.children:
+    #         if child.on_touch_up(touch):
+    #             break
+    #
+    #     self.selected_block = None
+    #
+    # def snap_to_grid(self, child):
+    #     x, y = child.pos
+    #
+    #     # Snap child to grid positions.
+    #     # There is probably a better way?
+    #     x_grid = x // self.grid_scale
+    #     y_grid = y // self.grid_scale
+    #     x = x_grid * self.grid_scale if x % self.grid_scale < self.half_scale else (x_grid + 1) * self.grid_scale
+    #     y = y_grid * self.grid_scale if y % self.grid_scale < self.half_scale else (y_grid + 1) * self.grid_scale
+    #
+    #     # Keep child inside of border
+    #     if x + child.width > self.width:
+    #         x = self.width - child.width
+    #     if x < 0:
+    #         x = 0
+    #     if y + child.height > self.height:
+    #         y = self.height - child.height
+    #     if y < 0:
+    #         y = 0
+    #     child.pos = x, y
